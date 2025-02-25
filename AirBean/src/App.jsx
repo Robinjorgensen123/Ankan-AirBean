6f0ac3ccbb2cd5a06de3466bd31bc1403b4a930c
robjor88 <robjor88@gmail.com>
2025-02-25 10:12:47 +0100

    testing routing

--- AirBean/src/App.jsx
+++ AirBean/src/App.jsx
@@ -1,16 +1,13 @@
-import Header from './Components/Header/Header'
+import { useState } from 'react'
 import './App.css'
-import About from './pages/About/about' 
-
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-   <Header />
     <div className='app-container'>
-    <About />
+
     </div>
     </>
   )
