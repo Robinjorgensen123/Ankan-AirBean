d8bb0248804f1c80f0b15d89bf0be13e3e083f7c
robjor88 <robjor88@gmail.com>
2025-02-27 12:46:02 +0100

    added the api data to statusPage

--- AirBean/src/pages/Status/Status.jsx
+++ AirBean/src/pages/Status/Status.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Importera useNavigate
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'; // Importera useNavigate
+import { getOrderStatus } from '../../components/Api/apiService';
 import droneImg from "../../assets/Images/drone.png";
 import "./Status.scss";
 
 const Status = () => {
+
+  const [orderStatus, setOderStatus] = useState(null) // lagra status från APIet
+  const { orderNr } = useParams()
   const navigate = useNavigate(); // Skapa navigate-funktionen
+
+  useEffect(() => {
+    const fetchOrderStatus = async () => {
+      try {
+        const statusData = await getOrderStatus(orderNr)
+        setOderStatus(statusData)
+      } catch (err) {
+        setError("cannot get orderstatus. Please try again later")
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if (orderNr) {
+      fetchOrderStatus()
+    }
+  }, [orderNr])
+
+  
+  const formatTime = (eta) => {
+    if (eta === 0) {
+      return "The order has been delivered" // om tiden är 0, visa Leverarad status
+    }
+    return `${eta} minutes`; // visa tiden i minuter om den ej är 0
+  }
 
   const handleClick = () => {
   navigate('/menu');  /// När knappen trycks, navigera till /about
@@ -13,13 +42,13 @@ const Status = () => {
   return (
     <article className="status-container">
       <div className="status-card">
-        <p>Ordernummer </p>
+        <p>Ordernummer:{orderNr}{/*visa ordernummer */} </p>
         <img className="drone-img"  
           src={droneImg} 
           alt="drone" 
         />
         <h1>Din beställning <br /> är påväg!</h1>
-        <p>test</p>
+        <p>{formatTime(orderStatus?.eta)}</p>
         <button className="cool-btn" onClick={handleClick}>Ok, cool!</button>
       </div>
     </article>
