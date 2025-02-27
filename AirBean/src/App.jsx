d40a372df48425e74901efecbd11ff1bddf5b84e
Camilla Lundahl <Camillalundahl@outlook.com>
2025-02-27 09:16:07 +0100

    skapat menysida med knappar

--- AirBean/src/App.jsx
+++ AirBean/src/App.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';  // Importera Router
 import Routing from './Components/Routing/Routing'; // Importera Routing-komponenten
 import About from './pages/About/about';
-import Header from "./Components/Header/Header";
 import OrderForm from "./components/Api/OrderForm";
 import OrderStatus from "./components/Api/OrderStatus";
 import CartManager from "./components/CartOverlay/CartManager";
 import DataDisplay from "./components/Api/Coffemenu";
 import "./App.css";
+import Menu from './pages/Menu/menu';
 
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
       <Router> 
       <div className="app-container">
         <Routing/>
         <DataDisplay />
         <CartManager />
         <About />
+        <Menu />
         <OrderForm />
         <OrderStatus />
       </div>
