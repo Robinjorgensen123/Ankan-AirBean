8f807233b4531b3d8b97aef61d45aecae01e7e7f
Alexander <aleck_johansson@hotmail.com>
2025-02-27 19:11:04 +0100

    cartoverlay styling and other small fixes

--- AirBean/src/components/CartOverlay/CartOverlay.jsx
+++ AirBean/src/components/CartOverlay/CartOverlay.jsx
@@ -5,13 +5,8 @@ function CartOverlay({ isVisible, toggleOverlay }) {
   if (!isVisible) return null;
 
   return (
-    <div
-      className="cart-overlay-background"
-    >
-      <div
-        className="cart-overlay-content"
-        onClick={(e) => e.stopPropagation()} 
-      >
+    <div className="cart-overlay-background">
+      <div className="cart-overlay-content" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={toggleOverlay}>X</button>
         <h2 className="h2-overlay">Din beställning</h2>
         <p>Varor här</p>
