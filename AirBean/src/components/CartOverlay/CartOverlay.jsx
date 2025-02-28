7d23185f30d85210678243f92ce2a45b6f53810d
Camilla Lundahl <Camillalundahl@outlook.com>
2025-02-28 11:52:48 +0100

    uppdaterat att antal tickar ner när man tar bort en vara

--- AirBean/src/components/CartOverlay/CartOverlay.jsx
+++ AirBean/src/components/CartOverlay/CartOverlay.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getCartItems, removeFromCart, clearCart } from '../../utils/cartUtils';
+import { getCartItems, removeFromCart } from '../../utils/cartUtils';
 import './CartOverlay.scss';
 
-function CartOverlay({ isVisible, toggleOverlay }) {
+function CartOverlay({ isVisible, toggleOverlay, setStoredAntalIKundvagn }) {
   if (!isVisible) return null;
   console.log('CartOverlay is visible');
   
@@ -19,12 +19,15 @@ function CartOverlay({ isVisible, toggleOverlay }) {
   
   // Funktion för att ta bort en artikel
   const handleRemoveItem = (index) => {
-    removeFromCart(index);
-    // Tvinga omrendering
-    toggleOverlay();
-    toggleOverlay();
+    // Ta bort varan från localStorage
+    const updatedItems = removeFromCart(index);
+
+    // Uppdatera antalet varor i Header
+    setStoredAntalIKundvagn(updatedItems.length);  // Uppdatera antalet varor i Header
+
+    // Ingen anledning att stänga overlayen här längre
   };
-  
+
   // Beräkna totalsumma
   const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0);
 
@@ -41,18 +44,17 @@ function CartOverlay({ isVisible, toggleOverlay }) {
             <ul>
               {cartItems.map((item, index) => (
                 <li key={index}>
-                <div className="cart-item-content">
-                  <div className="cart-item-title">{item.title}</div>
-                  <div className="cart-item-price">{item.price} kr</div>
-                </div>
-                <button className="remove-button" onClick={() => handleRemoveItem(index)}>Ta bort</button>
-              </li>
+                  <div className="cart-item-content">
+                    <div className="cart-item-title">{item.title}</div>
+                    <div className="cart-item-price">{item.price} kr</div>
+                  </div>
+                  <button className="remove-button" onClick={() => handleRemoveItem(index)}>Ta bort</button>
+                </li>
               ))}
             </ul>
             
             <div className="total-row">
               <span className="total-text">Total:</span>
-            
               <span className="total-price">{totalPrice} kr</span>
             </div>
             <p className="include">inkl moms + drönarleverans</p>
